1602d402dd67fe77fa820a8402a9622d2a044181
EungiKim <50321812+fotool@users.noreply.github.com>
2019-10-01 10:32:30 +0900

    191001

--- PhoneSample/app/src/main/java/com/example/phonesample/MainActivity.java
+++ PhoneSample/app/src/main/java/com/example/phonesample/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.phonesample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 객체 연결
+        et_tel = findViewById(R.id.et_tel);
+        et_tel.setText("Hello");
+
+    }
+
+    // 버튼 이벤트
+    public void OnBtnClick(View v) {
+        String data = et_tel.getText().toString();      // toString() : 받은 값을 문자열로 바꿔줌
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+        
     }
 }
